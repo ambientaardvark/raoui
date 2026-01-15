ed12133b45b64160cd56aa318f53d8ded9f27009
Alan-Lee <lee.chris.h.alan@gmail.com>
2026-01-14 21:00:12 -0800

    refactor to store tty cursor position in state

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,5 @@
 let prompt = "raoui> "
 
-(* Terminal cursor helpers *)
-let cursor_up n = if n = 0 then "" else Printf.sprintf "\x1b[%dA" n
-let cursor_down n = if n = 0 then "" else Printf.sprintf "\x1b[%dB" n
-let cursor_right n = if n = 0 then "" else Printf.sprintf "\x1b[%dC" n
-let cursor_left n = if n = 0 then "" else Printf.sprintf "\x1b[%dD" n
-
 let set_raw_mode () =
   let termio = Unix.tcgetattr Unix.stdin in
   let raw_termio = { termio with
@@ -23,14 +17,42 @@ let restore_mode termio =
 (* Model *)
 type state = {
   lines : string list;
-  cursor_line : int;
-  cursor_col : int;
+  cursor_row : int;       (* relative to prompt box, 0 = top *)
+  cursor_col : int;       (* relative to after prompt, 0 = first char *)
   output : string option;
-  prompt_height : int;
+  prompt_top_row : int;   (* absolute terminal row of prompt box top, 1-indexed *)
   term_width : int;
+  term_height : int;
 }
 
-let init = { lines = [""]; cursor_line = 0; cursor_col = 0; output = None; prompt_height = 1; term_width = 80 }
+(* Query cursor position using ANSI escape sequence *)
+let get_cursor_position () =
+  print_string "\x1b[6n";
+  flush stdout;
+  (* Response is ESC [ row ; col R *)
+  let buf = Buffer.create 16 in
+  let rec read_until_r () =
+    let c = input_char stdin in
+    if c = 'R' then ()
+    else (Buffer.add_char buf c; read_until_r ())
+  in
+  read_until_r ();
+  let response = Buffer.contents buf in
+  (* Parse "ESC[row;col" *)
+  Scanf.sscanf response "\x1b[%d;%d" (fun row col -> (row, col))
+
+let make_init () =
+  let (row, _col) = get_cursor_position () in
+  let term_height = match Terminal_size.get_rows () with
+    | Some h -> h
+    | None -> failwith "can't get terminal height"
+  in
+  let term_width = match Terminal_size.get_columns () with
+    | Some w -> w
+    | None -> failwith "can't get terminal width"
+  in
+  { lines = [""]; cursor_row = 0; cursor_col = 0; output = None;
+    prompt_top_row = row; term_width; term_height }
 
 (* Wrapping and coordinate mapping *)
 let wrap_line width line =
@@ -57,123 +79,129 @@ let internal_to_terminal width lines (line_idx, col) =
   let col_in_line = col mod width in
   (rows_before + row_in_line, col_in_line)
 
-(* Helpers *)
-let current_line state = List.nth state.lines state.cursor_line
+let terminal_to_internal width lines (row, col) =
+  let rec find_line line_idx rows_consumed remaining_lines =
+    match remaining_lines with
+    | [] -> failwith "terminal_to_internal: row out of bounds"
+    | line :: rest ->
+      let wrapped_count = List.length (wrap_line width line) in
+      if row < rows_consumed + wrapped_count then
+        (* cursor is within this line *)
+        let row_within_line = row - rows_consumed in
+        let col_within_line = row_within_line * width + col in
+        (line_idx, col_within_line)
+      else
+        find_line (line_idx + 1) (rows_consumed + wrapped_count) rest
+  in
+  find_line 0 0 lines
 
-let update_current_line state new_line =
-  let new_lines = List.mapi (fun i line ->
-    if i = state.cursor_line then new_line else line
-  ) state.lines in
-  { state with lines = new_lines }
+(* Helpers *)
+let get_line lines line_idx = List.nth lines line_idx
+
+let update_line lines line_idx new_line =
+  List.mapi (fun i line ->
+    if i = line_idx then new_line else line
+  ) lines
 
 (* Update *)
+let effective_width state = state.term_width - String.length prompt
+
 let insert_char state c =
-  let line = current_line state in
-  let before = String.sub line 0 state.cursor_col in
-  let after = String.sub line state.cursor_col (String.length line - state.cursor_col) in
+  let width = effective_width state in
+  let (line_idx, col) = terminal_to_internal width state.lines (state.cursor_row, state.cursor_col) in
+  let line = get_line state.lines line_idx in
+  let before = String.sub line 0 col in
+  let after = String.sub line col (String.length line - col) in
   let new_line = before ^ String.make 1 c ^ after in
-  { (update_current_line state new_line) with cursor_col = state.cursor_col + 1 }
+  let new_lines = update_line state.lines line_idx new_line in
+  let (new_row, new_col) = internal_to_terminal width new_lines (line_idx, col + 1) in
+  { state with lines = new_lines; cursor_row = new_row; cursor_col = new_col }
 
 let delete_char state =
-  if state.cursor_col = 0 then
-    if state.cursor_line = 0 then state
-    else (
+  let width = effective_width state in
+  let (line_idx, col) = terminal_to_internal width state.lines (state.cursor_row, state.cursor_col) in
+  if col = 0 then
+    if line_idx = 0 then state
+    else
       (* Join with previous line *)
-      print_string (cursor_up 1);
-      let prev_line = List.nth state.lines (state.cursor_line - 1) in
-      let curr_line = current_line state in
+      let prev_line = get_line state.lines (line_idx - 1) in
+      let curr_line = get_line state.lines line_idx in
       let merged = prev_line ^ curr_line in
       let new_lines = state.lines
-        |> List.filteri (fun i _ -> i <> state.cursor_line)
-        |> List.mapi (fun i line -> if i = state.cursor_line - 1 then merged else line)
+        |> List.filteri (fun i _ -> i <> line_idx)
+        |> List.mapi (fun i line -> if i = line_idx - 1 then merged else line)
       in
-      { state with lines = new_lines; cursor_line = state.cursor_line - 1; cursor_col = String.length prev_line }
-    )
+      let (new_row, new_col) = internal_to_terminal width new_lines (line_idx - 1, String.length prev_line) in
+      { state with lines = new_lines; cursor_row = new_row; cursor_col = new_col }
   else
-    let line = current_line state in
-    let before = String.sub line 0 (state.cursor_col - 1) in
-    let after = String.sub line state.cursor_col (String.length line - state.cursor_col) in
-    { (update_current_line state (before ^ after)) with cursor_col = state.cursor_col - 1 }
+    let line = get_line state.lines line_idx in
+    let before = String.sub line 0 (col - 1) in
+    let after = String.sub line col (String.length line - col) in
+    let new_lines = update_line state.lines line_idx (before ^ after) in
+    let (new_row, new_col) = internal_to_terminal width new_lines (line_idx, col - 1) in
+    { state with lines = new_lines; cursor_row = new_row; cursor_col = new_col }
 
 
 let insert_newline state =
-  let line = current_line state in
-  let before = String.sub line 0 state.cursor_col in
-  let after = String.sub line state.cursor_col (String.length line - state.cursor_col) in
+  let width = effective_width state in
+  let (line_idx, col) = terminal_to_internal width state.lines (state.cursor_row, state.cursor_col) in
+  let line = get_line state.lines line_idx in
+  let before = String.sub line 0 col in
+  let after = String.sub line col (String.length line - col) in
   let new_lines =
     List.concat_map (fun (i, l) ->
-      if i = state.cursor_line then [before; after] else [l]
+      if i = line_idx then [before; after] else [l]
     ) (List.mapi (fun i l -> (i, l)) state.lines)
   in
-  let prompt_width = String.length prompt in
-  let effective_width = state.term_width - prompt_width in
-  let (cursor_row, cursor_col) = internal_to_terminal effective_width state.lines (state.cursor_line, state.cursor_col) in
-  [
-    cursor_up cursor_row;
-    cursor_left cursor_col;
-    "\n";
-    cursor_down cursor_row;
-    cursor_right cursor_col
-  ]
-  |> String.concat ""
-  |> print_string;
-  { state with lines = new_lines; cursor_line = state.cursor_line + 1; cursor_col = 0; prompt_height = state.prompt_height + 1 }
+  let (new_row, new_col) = internal_to_terminal width new_lines (line_idx + 1, 0) in
+  { state with lines = new_lines; cursor_row = new_row; cursor_col = new_col }
 
 let move_left state =
+  let width = effective_width state in
   if state.cursor_col > 0 then
     { state with cursor_col = state.cursor_col - 1 }
-  else if state.cursor_line > 0 then
-    let prev_line = List.nth state.lines (state.cursor_line - 1) in
-    { state with cursor_line = state.cursor_line - 1; cursor_col = String.length prev_line }
+  else if state.cursor_row > 0 then
+    { state with cursor_row = state.cursor_row - 1; cursor_col = width - 1 }
   else
     state
 
 let move_right state =
-  let line = current_line state in
-  if state.cursor_col < String.length line then
+  let width = effective_width state in
+  let total_rows = List.length (wrap_lines width state.lines) in
+  let (line_idx, col) = terminal_to_internal width state.lines (state.cursor_row, state.cursor_col) in
+  let line = get_line state.lines line_idx in
+  (* Check if we're at the end of the current logical line *)
+  if col >= String.length line then
+    if line_idx < List.length state.lines - 1 then
+      (* Move to start of next line *)
+      let (new_row, new_col) = internal_to_terminal width state.lines (line_idx + 1, 0) in
+      { state with cursor_row = new_row; cursor_col = new_col }
+    else
+      state
+  else if state.cursor_col < width - 1 then
     { state with cursor_col = state.cursor_col + 1 }
-  else if state.cursor_line < List.length state.lines - 1 then
-    { state with cursor_line = state.cursor_line + 1; cursor_col = 0 }
+  else if state.cursor_row < total_rows - 1 then
+    { state with cursor_row = state.cursor_row + 1; cursor_col = 0 }
   else
     state
 
 let move_up state =
-  let at_top_of_line = state.cursor_col < state.term_width in
-  if at_top_of_line then
-    if state.cursor_line = 0 then
-      state
-    else
-      let last_line_len = (state.cursor_line - 1) |> List.nth state.lines |> String.length in
-      print_string (cursor_up 1);
-      { state with
-        cursor_line = state.cursor_line - 1;
-        cursor_col = (last_line_len / state.term_width) * state.term_width + state.cursor_col
-      }
-  else (
-    print_string (cursor_up 1);
-    { state with cursor_col = state.cursor_col - state.term_width}
-  )
+  if state.cursor_row > 0 then
+    { state with cursor_row = state.cursor_row - 1 }
+  else
+    state
 
 let move_down state =
-  let line = current_line state in
-  let at_bottom_of_line = state.cursor_col + state.term_width >= String.length line in
-  if at_bottom_of_line then
-    if state.cursor_line = List.length state.lines - 1 then
-      state
-    else
-      let col_offset = state.cursor_col mod state.term_width in
-      print_string (cursor_down 1);
-      { state with
-        cursor_line = state.cursor_line + 1;
-        cursor_col = col_offset
-      }
-  else (
-    print_string (cursor_down 1);
-    { state with cursor_col = state.cursor_col + state.term_width }
-  )
+  let width = effective_width state in
+  let total_rows = List.length (wrap_lines width state.lines) in
+  if state.cursor_row < total_rows - 1 then
+    { state with cursor_row = state.cursor_row + 1 }
+  else
+    state
 
 let submit state =
   let text = String.concat "\n" state.lines in
+  let init = make_init () in
   { init with output = Some text }
 
 let update key state =
@@ -197,46 +225,66 @@ let update key state =
   | _ -> Some state
 
 (* View *)
-let go_to_start = "\r"
 let clear_line = "\x1b[K"
-let cursor_column col = Printf.sprintf "\x1b[%dG" col
+let cursor_to row col = Printf.sprintf "\x1b[%d;%dH" row col
 
-let print_output text cursor_row total_rows =
-  let lines_to_drop = cursor_down (total_rows - cursor_row + 5) in
-  print_endline (go_to_start ^ lines_to_drop ^ text)
+let print_output state text total_rows =
+  (* Print output below prompt box *)
+  let output_row = state.prompt_top_row + total_rows in
+  print_string (cursor_to output_row 1);
+  print_endline text
 
 let view state =
   let prompt_width = String.length prompt in
-  let effective_width = state.term_width - prompt_width in
-  let wrapped = wrap_lines effective_width state.lines in
+  let width = effective_width state in
+  let wrapped = wrap_lines width state.lines in
   let total_rows = List.length wrapped in
-  let (cursor_row, cursor_col_in_wrap) =
-    internal_to_terminal effective_width state.lines (state.cursor_line, state.cursor_col)
+  (* Check if prompt would overflow terminal bottom, scroll if needed *)
+  let prompt_bottom = state.prompt_top_row + total_rows - 1 in
+  let overflow = prompt_bottom - state.term_height in
+  let state =
+    if overflow > 0 then (
+      (* Scroll terminal by printing newlines at bottom *)
+      print_string (cursor_to state.term_height 1);
+      for _ = 1 to overflow do
+        print_string "\n"
+      done;
+      { state with prompt_top_row = state.prompt_top_row - overflow }
+    )
+    else state
   in
+  (* Adjust prompt_top_row if cursor would be above visible area *)
+  (* cursor is at terminal row (prompt_top_row + cursor_row), needs to be >= 1 *)
+  let state =
+    if state.prompt_top_row + state.cursor_row < 1 then
+      { state with prompt_top_row = 1 - state.cursor_row }
+    else state
+  in
+  (* Compute viewport: where we actually start rendering *)
+  (* viewport_start is at least 1 (top of terminal) *)
+  let viewport_start = max 1 state.prompt_top_row in
+  (* skip_rows: how many rows of prompt are above viewport_start *)
+  let skip_rows = viewport_start - state.prompt_top_row in
+  let visible_rows = List.filteri (fun i _ -> i >= skip_rows) wrapped in
   (match state.output with
-   | Some text -> print_output text cursor_row total_rows
+   | Some text -> print_output state text total_rows
    | None -> ());
-  (* Move to top of prompt area *)
-  print_string (cursor_up cursor_row);
-  print_string go_to_start;
-  (* Print each wrapped line *)
+  (* Move to top of visible area *)
+  print_string (cursor_to viewport_start 1);
+  (* Print each visible wrapped line *)
   List.iteri (fun i line ->
     print_string clear_line;
-    if i = 0 then print_string prompt
+    if i + skip_rows = 0 then print_string prompt
     else print_string (String.make prompt_width ' ');
     print_string line;
-    if i < total_rows - 1 then print_string "\n"
-  ) wrapped;
-  (* Clear any leftover lines from previous render *)
-  for _ = total_rows to state.prompt_height - 1 do
-    print_string ("\n" ^ clear_line)
-  done;
-  (* Position cursor *)
-  let rows_from_bottom = total_rows - 1 - cursor_row in
-  print_string (cursor_up rows_from_bottom);
-  print_string (cursor_column (prompt_width + cursor_col_in_wrap + 1));
+    if i < List.length visible_rows - 1 then print_string "\n"
+  ) visible_rows;
+  (* Position cursor using absolute positioning *)
+  let cursor_abs_row = state.prompt_top_row + state.cursor_row in
+  let cursor_abs_col = prompt_width + state.cursor_col + 1 in
+  print_string (cursor_to cursor_abs_row cursor_abs_col);
   flush stdout;
-  { state with prompt_height = total_rows }
+  state
 
 (* Main loop *)
 let run () =
@@ -246,7 +294,7 @@ let run () =
     | None -> ()
     | Some state -> loop state
   in
-  loop init
+  loop (make_init ())
 
 let () =
   let orig = set_raw_mode () in
