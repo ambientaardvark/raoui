66096c015cacda44382d3800b47de82ecafff78f
Alan-Lee <lee.chris.h.alan@gmail.com>
2026-02-02 18:34:12 -0800

    add vscode initialization to startup script

--- r_scripts/startup.R
+++ r_scripts/startup.R
@@ -1,2 +1,7 @@
 options(cli.num_colors = 256)
 options(crayon.enabled = TRUE)
+
+# VSCode integration
+# if (Sys.getenv("TERM_PROGRAM") == "vscode" && Sys.getenv("VSCODE_INIT_R") != "") {
+#     source(Sys.getenv("VSCODE_INIT_R"), chdir = TRUE)
+# }
